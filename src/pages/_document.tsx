41ee36d471b4e67a1dce74bf305b7d268054f27e
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-05 12:13:44 -0300

    chore: add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <title>Bolão da Copa do Mundo de 2022</title>
       </Head>
       <body className="bg-gray-900 bg-bg-cover bg-cover bg-no-repeat">
         <Main />
